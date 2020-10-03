a8e9f319b9baf8efa94a30cb25a8687cb98334e2
TriptoAfsin <afsintripto@gmail.com>
2020-10-03 22:13:17 +0600

    Updates

---
+++ precache-manifest.dbeffe88706a456e057ccacc0a47efec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62c16246bdddae685ffd06cc67b0051",
+    "url": "/LyrReact/index.html"
+  },
+  {
+    "revision": "66b3238db2eba022f241",
+    "url": "/LyrReact/static/css/main.20c34f78.chunk.css"
+  },
+  {
+    "revision": "48e21968cfae14511522",
+    "url": "/LyrReact/static/js/2.4ab195d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LyrReact/static/js/2.4ab195d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b3238db2eba022f241",
+    "url": "/LyrReact/static/js/main.18aa8eb9.chunk.js"
+  },
+  {
+    "revision": "da10ab73757019163e5e",
+    "url": "/LyrReact/static/js/runtime-main.d2912059.js"
+  },
+  {
+    "revision": "3bd3d6a5ff0e2a0a3f85d935d9777e79",
+    "url": "/LyrReact/static/media/spinner1.3bd3d6a5.gif"
+  }
+]);
